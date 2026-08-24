0d6a3f3ebe5aeb1db1f3b9656d76313fb1e5b71b
Vishveshwara Mathkunti <vishwanm2006@gmail.com>
2026-08-24 10:07:38 +0530

    Implement NTP time synchronization function
    Added NTP synchronization function and time zone info. The simplest module everr..

--- src/mywifi.cpp
+++ src/mywifi.cpp
@@ -1,6 +1,23 @@
 #include "mywifi.h"
-
+#include "mytime.h"
 /**
  * TODO
  * WRITE YOUR CLASS FUNCTION IMPLEMENTATIONS HERE
  */
+const char* TZ_INFO = "IST-5:30"; 
+
+
+void syncTimeWithNTP() {
+    // 1. Set offset (5.5 hours * 3600 seconds) and NTP server
+    configTime(5.5 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+    struct tm timeinfo;
+    // 2. Try to get the local time from the internal ESP32 clock
+    if (getLocalTime(&timeinfo)) {
+        // 3. Write this valid time into your PCF8563 RTC hardware
+        rtc_set_time(&timeinfo); 
+        Serial.println("RTC synced with NTP");
+    } else {
+        Serial.println("NTP Sync Failed");
+    }
+}
