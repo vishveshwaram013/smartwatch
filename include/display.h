07e3ca4d8d3f4a4d5c03f268cb49b86c7e24f7f0
Vishveshwara Mathkunti <vishwanm2006@gmail.com>
2026-08-24 10:11:07 +0530

    Add new display methods to Display class
    Added new display functions for drawing strings, date/time, and stopwatch.

--- include/display.h
+++ include/display.h
@@ -6,16 +6,21 @@
 class Display
 {
 public:
+    void setupDisplay();
+    void clearDisplay();
+    void drawCircle(int x, int y, int radius, uint16_t color);
     void drawWiFiSymbol(int x, int y, uint16_t color = TFT_WHITE);
     void drawHomeSymbol(int x, int y, uint16_t color = TFT_WHITE);
     void drawRefreshSymbol(int x, int y, uint16_t color = TFT_WHITE);
     void drawClockSymbol(int x, int y, uint16_t color = TFT_WHITE);
     void drawFitnessSymbol(int x, int y, uint16_t color = TFT_WHITE);
     void drawBatterySymbol(int x, int y, int battery, uint16_t color = TFT_WHITE);
-    /**
-     * TODO
-     * WRITE YOUR PUBLIC MEMBERS AND FUNCTION HEADERS HERE
-     */
+
+    void drawString(String text, int x, int y, int color = TFT_WHITE);
+    void drawStringg(String text, int x, int y, int size);
+    void drawDateTimeLock(struct tm *timeinfo);
+    void drawTestInRectable(String s, int x, int y, uint16_t color=TFT_WHITE);
+    void drawStopWatchTime(int seconds, int minutes, int hours);
 
 private:
     TFT_eSPI tft;
