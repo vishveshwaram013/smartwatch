31cc1c433145275f5be0c817904e68395f6c6cb2
Vishveshwara Mathkunti <vishwanm2006@gmail.com>
2026-08-24 10:14:10 +0530

    Add I2C address definitions and RTC helper functions

--- include/mytime.h
+++ include/mytime.h
@@ -13,10 +13,16 @@ extern "C"
 {
 #endif
 
-void syncTimeWithNTP();
+#define PCF8563_ADDR  0x51  //i2c address for PCF8563
+#define FT6336U_ADDR  0x38
+
+
 uint8_t decToBcd(uint8_t val);
 uint8_t bcdToDec(uint8_t val);
 
+//helper functions
+esp_err_t rtc_get_time(struct tm *time_ptr);
+esp_err_t rtc_set_time(struct tm *time_ptr);
 
 #ifdef __cplusplus
 }
